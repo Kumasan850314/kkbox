5320745e6cf47964744678d7b6e1e530660462a8
kuma <kumasan850314@gmail.com>
2021-08-24 08:18:07 +0800

    first

---
+++ 0823add.test.js
@@ -0,0 +1,15 @@
+/* step record
+    1.npm init 
+    2.update package.json 【"test": "jest"】
+    3.npm run test
+*/
+
+function add(a, b) {
+    return a+b;
+}
+
+describe('add function test', function(){
+    it('5+10=15', function(){
+        expect(add(5, 10)).toEqual(15) // toBe or toEqual
+    })    
+})
